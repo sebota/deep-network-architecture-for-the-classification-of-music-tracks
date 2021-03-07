42dd5f8fb656e2d8fc493e2517a05869b33e0ee0
Sebastian <sebotawski@gmail.com>
2021-03-07 22:01:54 +0100

    delete code

--- models.py
+++ models.py
@@ -7,11 +7,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow.keras import regularizers
 
-p = {'neuron_first': [128],
-     'neuron_second': [16, 32, 64],
-     'batch_size': [64, 128],
-     'dropout': [0, 0.2, 0.4]}
-
 
 # def model_seq(x_train_med, y_train_med, x_test_med, y_test_med, x_val_med, y_val_med):
 #     model = tf.keras.models.Sequential([
@@ -307,70 +302,3 @@ def model_cnn_rnn(x_train_med, y_train_med, x_test_med, y_test_med, x_val_med, y
 
     plot(history)
     model.save('model_cnn_rnn.h5')
-
-# def model_cnn_spec(x_train, y_train, x_val, y_val):
-#     batch_size = 32
-#     num_classes = 8
-#     n_features = x_train.shape[2]
-#     n_time = x_train.shape[1]
-#
-#     N_LAYERS = 3
-#     FILTER_LENGTH = 5
-#     CONV_FILTER_COUNT = 56
-#     BATCH_SIZE = 32
-#     LSTM_COUNT = 96
-#     EPOCH_COUNT = 70
-#     NUM_HIDDEN = 64
-#     L2_regularization = 0.001
-#
-#     # input_shape = (273, 1)
-#     # model_input = tf.keras.layers.Input(input_shape, name='input')
-#     # layer = model_input
-#
-#     n_features = x_train.shape[2]
-#     input_shape = (None, n_features)
-#     model_input = tf.keras.layers.Input(input_shape, name='input')
-#     layer = model_input
-#
-#     ### 3 1D Convolution Layers
-#     for i in range(N_LAYERS):
-#         # give name to the layers
-#         layer = tf.keras.layers.Conv1D(
-#             filters=CONV_FILTER_COUNT,
-#             kernel_size=FILTER_LENGTH,
-#             kernel_regularizer=regularizers.l2(L2_regularization),  # Tried 0.001
-#             name='convolution_' + str(i + 1)
-#         )(layer)
-#         layer = tf.keras.layers.BatchNormalization(momentum=0.9)(layer)
-#         layer = tf.keras.layers.Activation('relu')(layer)
-#         layer = tf.keras.layers.MaxPooling1D(2)(layer)
-#         layer = tf.keras.layers.Dropout(0.4)(layer)
-#
-#     ## LSTM Layer
-#     layer = tf.keras.layers.LSTM(LSTM_COUNT, return_sequences=False)(layer)
-#     layer = tf.keras.layers.Dropout(0.4)(layer)
-#
-#     ## Dense Layer
-#     layer = tf.keras.layers.Dense(NUM_HIDDEN, kernel_regularizer=regularizers.l2(L2_regularization), name='dense1')(layer)
-#     layer = tf.keras.layers.Dropout(0.4)(layer)
-#
-#     ## Softmax Output
-#     layer = tf.keras.layers.Dense(num_classes)(layer)
-#     layer = tf.keras.layers.Activation('softmax', name='output_realtime')(layer)
-#     model_output = layer
-#     model = tf.keras.models.Model(model_input, model_output)
-#
-#     opt = tf.keras.optimizers.Adam(lr=0.001)
-#     model.compile(
-#         loss='categorical_crossentropy',
-#         optimizer=opt,
-#         metrics=['accuracy']
-#     )
-#
-#     history = model.fit(x_train, y_train, epochs=20, validation_data=(x_val, y_val))
-#
-#     # print(model.evaluate(x_test_med, y_test_med, verbose=2))
-#     # print(model.evaluate(x_val_med, y_val_med, verbose=2))
-#
-#     plot(history)
-#     model.save('model_cnn_spec.h5')
